648d407393211c3742cb64791bd82562afe773f2
Dameon Mendoza <dameonmendoza@gmail.com>
2019-02-17 09:11:01 -0700

    ball and slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+import { BallResizer } from './BallResizer';
+
+function App() {
+  return (
+    <div className="App">
+    <BallResizer
+      initialSize={200}
+      minSize={150}
+      maxSize={250}
+      />
+    </div>
+  )
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
-serviceWorker.unregister();
+
